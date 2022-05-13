fce480034cc56a4b68e49bda1b287dbbc12930d3
bas0101 <a.s.bozhenko@gmail.com>
2022-05-13 18:11:40 +0300

    aded Task

---
+++ src/main/java/ua/edu/sumdu/j2se/bozhenko/tasks/Task.java
@@ -0,0 +1,11 @@
+package ua.edu.sumdu.j2se.bozhenko.tasks;
+
+public class Task {
+    public Task(String title, int time) {
+    }
+
+    public Task(String title, int start, int end, int interval) {
+    }
+    
+
+}
