3f379a701ed706961e240057608d4db9ac0f74b9
bas0101 <a.s.bozhenko@gmail.com>
2022-05-17 20:42:15 +0300

    test done

--- src/main/java/ua/edu/sumdu/j2se/bozhenko/tasks/Task.java
+++ src/main/java/ua/edu/sumdu/j2se/bozhenko/tasks/Task.java
@@ -1,5 +1,6 @@
 package ua.edu.sumdu.j2se.bozhenko.tasks;
 
+
 public class Task {
     private String title;
     private int time;
@@ -28,28 +29,35 @@ public class Task {
         this.title = title;
     }
 
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
     public int getTime() {
-        return time;
+       if(isRepeated()) {
+           return start;
+       } else {
+           return time;
+       }
     }
 
     public void setTime(int time) {
+        if(isRepeated()) {
+            this.interval = 0;
+        }
         this.time = time;
     }
 
     public int getStartTime() {
-        return start;
+        if(isRepeated()) {
+            return start;
+        } else {
+            return time;
+        }
     }
 
     public int getEndTime() {
-        return end;
+        if(isRepeated()) {
+            return end;
+        } else {
+            return time;
+        }
     }
 
     public int getRepeatInterval() {
@@ -59,12 +67,47 @@ public class Task {
     public void setTime(int start, int end, int interval) {
         this.start = start;
         this.end = end;
-
+        if (!isRepeated()) {
+            this.interval = interval;
+        }
     }
 
-    public boolean isRepeated(){
-        return interval;
+    public boolean isActive() {
+        return active;
     }
 
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 
+    public boolean isRepeated() {
+        int interval = getRepeatInterval();
+        return interval > 0;
+    }
+
+    public int nextTimeAfter (int current) {
+        if(isActive() & !isRepeated()) {
+            if (current >= this.time) {
+                return -1;
+            } else {
+                return this.time;
+            }
+        }
+        if(isActive() & isRepeated()) {
+            if(current < this.start) {
+                return start;
+            }
+            else if((this.end - current) < this.interval) {
+                return -1;
+            }
+            else {
+                int time = this.start;
+                while (current >= time) {
+                    time += this.interval;
+                }
+                return time;
+            }
+        }
+        return -1;
+    }
 }
